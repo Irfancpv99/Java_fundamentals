ad1d3402312fde1d402ff057bd9a55842dd6360e
Irfancpv99 <irfancpv99@gmail.com>
2024-09-28 16:06:35 +0200

    addition

---
+++ addition.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class addition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter the third number: ");
+        double num3 = scanner.nextDouble();
+
+        double sum = num1 + num2 + num3;
+
+        System.out.println("The sum of the two numbers is: " + sum);
+    }
+}
